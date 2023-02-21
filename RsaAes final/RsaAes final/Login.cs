a6ce2d4fb1cb04b9172c85f3a52100249fef170c
Mohamed Hussain <73284644+Mohamed6969@users.noreply.github.com>
2023-02-21 11:24:58 +0000

    Add files via upload
    Key derivation function and password recovery

---
+++ RsaAes final/RsaAes final/Login.cs
@@ -0,0 +1,59 @@
+﻿using MainGui;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RsaAes
+{
+    public partial class Login : Form
+    {
+        SqlCommand cmd;
+        SqlConnection cn;
+        SqlDataReader dr;
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+            //cn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""D:\RsaAes final\Database1.mdf"";Integrated Security=True");
+            //cn.Open();
+        }
+
+        private void BtnLogin_Click(object sender, EventArgs e)
+        {
+            //    if (textBoxpassword.Text != string.Empty || textBoxusername.Text != string.Empty)
+            //    {
+
+            //        cmd = new SqlCommand("select * from LoginTable where username='" + textBoxusername.Text + "' and password='" + textBoxpassword.Text + "'", cn);
+            //        dr = cmd.ExecuteReader();
+            //        if (dr.Read())
+            //        {
+            //            dr.Close();
+            this.Hide();
+            MainGui.GUI home = new GUI();
+            home.ShowDialog();
+            //        }
+            //        else
+            //        {
+            //            dr.Close();
+            //            MessageBox.Show("No Account avilable with this username and password ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //        }
+
+            //    }
+            //    else
+            //    {
+            //        MessageBox.Show("Please enter value in all field.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //    }
+        }
+
+    }
+}
